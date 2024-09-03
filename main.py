eaf690e26f37f41ae6f7476585af1102934885a8
Vinicius <viniaoliveira22@gmail.com>
2024-09-03 17:44:12 -0300

    feat: implementing get_cor and get_objetivo

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from jogador.preparacao.cor import *
+from jogador.preparacao.objetivos import *
 from fastapi import FastAPI
 app = FastAPI()
-
-
 
 
 @app.get("/")
 def read_hello():
     return "Hello world!"
+
+@app.get("/prepara/cor")
+async def get_color(select_cor:int):
+    return buscar_cor(select_cor)
+
+@app.get("/prepara/objetivo")
+async def get_objetivo():
+    return buscar_objetivo()
 
