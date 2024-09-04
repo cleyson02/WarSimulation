5c4d9aad6a877442f0b051a8422d8c324b5e508e
Cleyson Silva de Oliveira <cleysonsilva2002@gmail.com>
2024-09-04 00:31:38 -0300

    edição dos comentários

--- jogador/preparacao/distribuidor.py
+++ jogador/preparacao/distribuidor.py
@@ -1,23 +1,18 @@
 import random
 import cor
 
-# Gerando uma lista de números aleatórios de 1 a 6 sem repetição
 valores_aleatorios = random.sample(range(1, 7), 6)
 
-# Variável para armazenar a cor que receber o valor 6 primeiro
 distribuidor = None
 
-# Atribuindo valores aleatórios às cores
 for i in range(1, 7):
     cor_atual = cor.buscar_cor(i)
     valor_atual = valores_aleatorios[i - 1]
     
-    # Verifica se o valor é 6 e atribui à variável 'distribuidor'
     if valor_atual == 6 and distribuidor is None:
         distribuidor = cor_atual
     
     print(f"A cor {cor_atual} recebeu o valor {valor_atual}")
 
-# Exibindo o 'distribuidor'
 if distribuidor:
     print(f"\nO distribuidor e primeiro jogador é: {distribuidor}")
